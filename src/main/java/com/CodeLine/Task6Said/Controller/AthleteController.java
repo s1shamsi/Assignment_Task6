d94f56a2729528a59aa673b0343867a6fdd428e5
Said Alshamsi <eng.saidj@gmail.com>
2023-05-21 12:23:17 +0400

    'update5'

--- src/main/java/com/CodeLine/Task6Said/Controller/AthleteController.java
+++ src/main/java/com/CodeLine/Task6Said/Controller/AthleteController.java
@@ -1,4 +1,38 @@
 package com.CodeLine.Task6Said.Controller;
 
+import com.CodeLine.Task6Said.Model.Athlete;
+import com.CodeLine.Task6Said.Repositry.AthleteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class AthleteController {
+
+    @Autowired
+    private AthleteRepository athleteRepository;
+
+    @GetMapping("/athletes")
+    public List<Athlete> getAllAthletes() {
+        return (List<Athlete>) athleteRepository.findAll();
+    }
+
+    @GetMapping("/athletes/{id}")
+    public Athlete getAthleteById(@PathVariable Long id) {
+        return athleteRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/athletes/nationality/{nationality}")
+    public List<Athlete> getAthletesByNationality(@PathVariable String nationality) {
+        return athleteRepository.findByNationality(nationality);
+    }
+
+    @GetMapping("/athletes/sport/{sport}")
+    public List<Athlete> getAthletesBySport(@PathVariable String sport) {
+        return athleteRepository.findBySport(sport);
+    }
+
 }
--- src/main/java/com/CodeLine/Task6Said/Controller/EventController.java
+++ src/main/java/com/CodeLine/Task6Said/Controller/EventController.java
@@ -1,4 +1,38 @@
 package com.CodeLine.Task6Said.Controller;
 
+import com.CodeLine.Task6Said.Model.Event;
+import com.CodeLine.Task6Said.Repositry.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.util.List;
+
+@RestController
 public class EventController {
+    @Autowired
+    private EventRepository eventRepository;
+
+    @GetMapping("/events")
+    public Iterable<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    @GetMapping("/events/{id}")
+    public Event getEventById(@PathVariable Long id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping("/events/sport/{sport}")
+    public List<Event> getEventsBySport(@PathVariable String sport) {
+        return eventRepository.findBySport(sport);
+    }
+
+    @GetMapping("/events/daterange/{startDate}/{endDate}")
+    public List<Event> getEventsByDateRange(@PathVariable Date startDate, @PathVariable Date endDate) {
+        return eventRepository.findByStartDateBetween(startDate, endDate);
+    }
+
 }
