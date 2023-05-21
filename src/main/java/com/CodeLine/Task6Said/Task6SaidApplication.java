b4c458880489095261c623337e5fbbc57cecb09a
Said Alshamsi <eng.saidj@gmail.com>
2023-05-21 11:47:19 +0400

    'new_task'

---
+++ src/main/java/com/CodeLine/Task6Said/Task6SaidApplication.java
@@ -0,0 +1,13 @@
+package com.CodeLine.Task6Said;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task6SaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task6SaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodeLine/Task6Said/Task6SaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeLine.Task6Said;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task6SaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
