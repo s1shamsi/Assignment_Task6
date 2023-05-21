2e28639561ff744fbab97ba5a33b9cf995ded79f
Said Alshamsi <eng.saidj@gmail.com>
2023-05-21 12:13:55 +0400

    'update2'

--- src/main/java/com/CodeLine/Task6Said/Repositry/AthleteRepository.java
+++ src/main/java/com/CodeLine/Task6Said/Repositry/AthleteRepository.java
@@ -1,5 +1,15 @@
 package com.CodeLine.Task6Said.Repositry;
 
-public interface AthleteRepository {
+import com.CodeLine.Task6Said.Model.Athlete;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AthleteRepository extends CrudRepository<Athlete, Long> {
+
+    List<Athlete> findByNationality(String nationality);
+
+    List<Athlete> findBySport(String sport);
 
 }
+
