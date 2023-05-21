1ed6f6f328a85027fff79c23f875e999debe0c21
Said Alshamsi <eng.saidj@gmail.com>
2023-05-21 12:15:07 +0400

    'update3'

--- src/main/java/com/CodeLine/Task6Said/Repositry/EventRepository.java
+++ src/main/java/com/CodeLine/Task6Said/Repositry/EventRepository.java
@@ -1,4 +1,15 @@
 package com.CodeLine.Task6Said.Repositry;
 
-public interface EventRepository {
+import com.CodeLine.Task6Said.Model.Event;
+import org.springframework.data.repository.CrudRepository;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface EventRepository extends CrudRepository<Event, Long> {
+
+    List<Event> findBySport(String sport);
+
+    List<Event> findByStartDateBetween(Date startDate, Date endDate);
+
 }
