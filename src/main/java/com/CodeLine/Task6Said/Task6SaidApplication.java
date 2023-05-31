1339fb93a9842a6b9730349668611aa2e61781e4
s1shamsi <eng.saidj@gmail.com>
2023-05-31 16:24:31 +0400

    test

--- src/main/java/com/CodeLine/Task6Said/Task6SaidApplication.java
+++ src/main/java/com/CodeLine/Task6Said/Task6SaidApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Task6SaidApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Task6SaidApplication.class, args);
 	}
